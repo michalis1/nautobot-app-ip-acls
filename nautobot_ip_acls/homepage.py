6b08452269e6c245bdc53730b578f5296b23682d
Michael Milaitis <mmialtiis@space.gr>
2024-08-06 12:59:18 +0300

    first

---
+++ nautobot_ip_acls/homepage.py
@@ -0,0 +1,18 @@
+from nautobot.apps.ui import HomePageItem, HomePagePanel
+from nautobot.extras.models import Status
+
+layout = (
+    HomePagePanel(
+        name="Organization",
+        items=(
+            HomePageItem(
+                name="Statuses",
+                model=Status,
+                weight=300,
+                link="extras:status_list",
+                description="Customizable object statuses",
+                permissions=["extras.view_status"],
+            ),
+        ),
+    ),
+)
