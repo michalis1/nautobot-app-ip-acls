7ee269bd4c2e133f8ab19349505ad85dea40b91e
Michael Milaitis <mmialtiis@space.gr>
2024-08-29 14:02:22 +0300

    final code from book

--- nautobot_ip_acls/navigation.py
+++ nautobot_ip_acls/navigation.py
@@ -4,6 +4,29 @@ from nautobot.apps.ui import NavMenuGroup, NavMenuItem, NavMenuTab
 
 menu_items = (
     NavMenuTab(
+        name="IP ACLs",
+        weight=1000,
+        groups=(
+            NavMenuGroup(
+                name="ACLs",
+                items=(
+                    NavMenuItem(
+                        link="plugins:nautobot_ip_acls:acl_list",
+                        name="ACLs",
+                        permissions=["nautobot_ip_acls.view_acl"],
+                    ),
+                    NavMenuItem(
+                        link="plugins:nautobot_ip_acls:aclentry_list",
+                        name="ACL Entries",
+                        permissions=[
+                            "nautobot_ip_acls.view_aclentry"
+                        ],
+                    ),
+                ),
+            ),
+        ),
+    ),
+    NavMenuTab(
         name="IPAM",
         groups=(
             NavMenuGroup(
@@ -15,6 +38,21 @@ menu_items = (
                         name="Standard IP ACLs",
                         permissions=["ipam:view_ipaddress"],
                         buttons=(),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    NavMenuTab(
+        name="Devices",
+        groups=(
+            NavMenuGroup(
+                name="Devices",
+                items=(
+                    NavMenuItem(
+                        link="plugins:nautobot_ip_acls:device_list",
+                        name="IP ACL Audit",
+                        permissions=["dcim.view_device"],
                     ),
                 ),
             ),
--- nautobot_ip_acls/template_content.py
+++ nautobot_ip_acls/template_content.py
@@ -1,6 +1,46 @@
 """Module to change object details view."""
 
+from django.urls import reverse
 from nautobot.apps.ui import TemplateExtension
+
+
+# pylint: disable=W0223
+class IPACLDeviceTemplateExtension(TemplateExtension):
+    """Add IP ACL information to the Nautobot Device detail view."""
+
+    model = "dcim.device"
+
+    def right_page(self):
+        """Add content on the right side of the view."""
+        # Get the object that was provided as template context;
+        # in this case, the Device object itself.
+        device = self.context["object"]
+        # Render the config context of this Device as a Python dict:
+        config_context_data = device.get_config_context()
+        # Get the contents of the "access_lists" key in the dict:
+        acls = config_context_data.get("access_lists", {})
+        # Construct the HTML to contain this data
+        # Start a panel containing an unordered list:
+        output = """
+            <div class="panel panel-default">
+            <div class="panel-heading"><strong>IP ACLs</strong></div>
+            <div class="panel-body">
+            <ul>
+        """
+        # Add list entries based on the available data:
+        for acl_id, acl_entries in acls.items():
+            output += f"<li>ACL <code>{acl_id}</code> " f"with {len(acl_entries)} entries</li>"
+        # End the list and the panel
+        output += "</ul></div></div>"
+        return output
+
+    def detail_tabs(self):
+        return [
+            {
+                "title": "IP ACLs",
+                "url": reverse("plugins:nautobot_ip_acls:device_ip_acls", kwargs={"pk": self.context["object"].pk}),
+            },
+        ]
 
 
 # pylint: disable=W0223
@@ -23,4 +63,4 @@ class StatusContent(TemplateExtension):
         """
 
 
-template_extensions = [StatusContent]  # Important to include!
+template_extensions = [StatusContent, IPACLDeviceTemplateExtension]  # Important to include!
