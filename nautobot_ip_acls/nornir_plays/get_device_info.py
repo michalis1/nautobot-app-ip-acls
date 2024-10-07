330afdf722899fe489606ef7a8ac18e41f7d12bd
Michael Milaitis <mmialtiis@space.gr>
2024-10-07 15:17:33 +0300

    nornir

--- nautobot_ip_acls/nornir_plays/get_device_info.py
+++ nautobot_ip_acls/nornir_plays/get_device_info.py
@@ -19,43 +19,34 @@ import json
 
 
 # Create Logger
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 
 def run_get_facts(task, logger):
     """Run the napalm get_facts for a device and store the information in the DiffSync adapter."""
     obj = task.host.data["obj"]
     port = 22
-    logger.debug(f"Getting device info for {obj} {obj.primary_ip4}...")
+    logger.info(msg=f"Getting device info for {obj} {obj.primary_ip4}...", extra={"object": obj})
     if tcp_ping(obj.primary_ip4.host, port):
         results = task.run(task=netmiko_send_command, command_string="show version", use_textfsm=True)
-
         if results[0].failed:
             return results
         output = results[0].result[0]
-        logger.debug(
+        logger.info(
             f"Show version: {output['running_image']}",
         )
         # Loop through each task result for this host
         for task_result in results:
-            print(f"Task: {task_result.name}")
-            print(f"Result: {task_result.result}")
-            print(f"Failed: {task_result.failed}")
-            print(f"Changed: {task_result.changed}")
+            logger.info(msg=f"Task: {task_result.name} Result: {task_result.result}", extra={"object": obj})
+            # logger.debug(f"Result: {task_result.result}")
+            # logger.debug(f"Failed: {task_result.failed}")
+            # logger.debug(f"Changed: {task_result.changed}")
         return Result(host=task.host, result=output)
 
     else:
-        if obj.status.name != "Inventory":
-            logger.debug(
-                f"Device {obj} is unreachable and Status in Nautobot is {obj.status}.Changing status to offline..",
-            )
-            obj.status = Status.objects.get(name="Offline")
-            obj.validated_save()
-
-        else:
-            logger.debug(
-                f"Device {obj} is unreachable and Status in Nautobot is {obj.status}.Skiping changing status..",
-            )
+        logger.debug(
+            f"Device {obj} is unreachable and Status in Nautobot is {obj.status}.Skiping changing status..",
+        )
 
 
 def get_device_info(job, devices):
@@ -74,14 +65,14 @@ def get_device_info(job, devices):
             },
         },
     ) as nornir_obj:
-        results = nornir_obj.run(task=run_get_facts, name="GET DEVICE FACTS", logger=logger)
-        logger.debug("Completed getting info from devices.")
+        results = nornir_obj.run(task=run_get_facts, name="GET DEVICE FACTS", logger=job.logger)
+        job.logger.info("Completed getting info from devices.")
         for host, multi_result in results.items():
-            print(f"Host: {host}")
-            
+            # job.logger.info(f"Host: {host}")
             # Loop through each task result for this host
             for task_result in multi_result:
-                print(f"Task: {task_result.name}")
-                print(f"Result: {task_result.result}")
-                print(f"Failed: {task_result.failed}")
-                print(f"Changed: {task_result.changed}")
+                job.logger.info(f"Host: {host} Task: {task_result.name} Result: {task_result.result}")
+                #job.logger.info(f"{dir(task_result)}")
+                # job.logger.info(f"Result: {task_result.result}")
+                # job.logger.info(f"Failed: {task_result.failed}")
+                # job.logger.info(f"Changed: {task_result.changed}")
